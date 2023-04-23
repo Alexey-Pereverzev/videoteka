c65b1a143b5399c5268abe4c5fae750226dccba3
serg <livegrafics@rambler.ru>
2023-04-23 19:19:28 +0300

    Create project

---
+++ cabinet-order-service/src/main/java/ru/gb/cabinetorderservice/CabinetOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.cabinetorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinetOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabinetOrderServiceApplication.class, args);
+    }
+
+}
