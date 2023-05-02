83ec18ebd520f21afbf99e0beaaefd1e445a9d4a
Anna <a.dyakonu85@mail.ru>
2023-05-02 19:20:08 +0300

    lombok

--- api/src/main/java/ru/gb/api/dtos/dto/StringResponse.java
+++ api/src/main/java/ru/gb/api/dtos/dto/StringResponse.java
@@ -1,20 +1,14 @@
 package ru.gb.api.dtos.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StringResponse {
     private String value;
 
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
-    public StringResponse(String value) {
-        this.value = value;
-    }
-
-    public StringResponse() {
-    }
+    
 }
