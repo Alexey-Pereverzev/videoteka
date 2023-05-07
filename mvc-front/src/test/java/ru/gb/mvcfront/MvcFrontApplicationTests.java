585aed11767507cb3f16d6cf444314ed07b4efa5
serg <livegrafics@rambler.ru>
2023-05-07 09:05:34 +0300

    Создана MVC заготовка

---
+++ mvc-front/src/test/java/ru/gb/mvcfront/MvcFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.mvcfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
