7f11b186681aea64a7cc7f28318a6031fbeb1b30
alexey_pereverzev <aper-hari@list.ru>
2023-05-13 21:05:02 +0600

    task04_4_прикручиваем_дополнительное_меню_для_профиля_и_корзины - 13-05-2023 recovery

--- mvc-front/src/main/resources/static/main.js
+++ mvc-front/src/main/resources/static/main.js
@@ -9,21 +9,47 @@ angular.module('videoteka', ['ngStorage']).controller('mainController', function
                 delete $localStorage.videotekaUser;
                 $http.defaults.headers.common.Authorization = '';
             }
+
         } catch (e) {
         }
 
         if ($localStorage.videotekaUser) {
-            $http.defaults.headers.common.Authorization = 'Bearer ' + $localStorage.marchMarketUser.token;
+            $http.defaults.headers.common.Authorization = 'Bearer ' + $localStorage.videotekaUser.token;
+            // console.log($http.defaults.headers.common.Authorization)
         }
+
+        if (!$localStorage.videotekaGuestCartId) {
+            $http.get('http://localhost:5555/cart/api/v1/cart/generate')
+                .then(function (response) {
+                    $localStorage.videotekaGuestCartId = response.data.value;
+                    console.log($localStorage.videotekaGuestCartId)
+                });
+        }
+
+        $scope.jsfilms = jsfilms;
+        console.log($scope.jsfilms);
+
     }
 
     $scope.tryToAuth = function () {
-        console.log('Привет');
         $http.post('http://localhost:5555/auth/authenticate', $scope.user)
             .then(function successCallback(response) {
                 if (response.data.token) {
                     $http.defaults.headers.common.Authorization = 'Bearer ' + response.data.token;
-                    $localStorage.videotekaUser = {username: $scope.user.username, token: response.data.token};
+                    $scope.payload = JSON.parse(atob(response.data.token.split('.')[1]));
+                    $localStorage.videotekaUser = {
+                        username: $scope.user.username,
+                        token: response.data.token,
+                        role: response.data.role,
+                        userId: $scope.payload.sub
+                    };
+
+                    // console.log(111);
+                    // console.log($localStorage.videotekaUser.token);
+
+                    $scope.username = $localStorage.videotekaUser.username;
+
+                    $scope.userRole = response.data.role;
 
                     $scope.user.username = null;
                     $scope.user.password = null;
@@ -43,11 +69,24 @@ angular.module('videoteka', ['ngStorage']).controller('mainController', function
 
     $scope.isUserLoggedIn = function () {
         if ($localStorage.videotekaUser) {
+            $scope.videotekaUser=$localStorage.videotekaUser.username;
             return true;
         } else {
             return false;
         }
     };
+
+    // $scope.films = films;
+
+    $scope.addToCart = function (id, index) {
+        // $http.get('http://localhost:5555/cart/api/v1/cart/' + $localStorage.videotekaGuestCartId
+        //     + '/add/' + $scope.jsfilms[index])
+        //     .then(function (response) {
+        //     });
+        console.log(id);
+        console.log(index);
+    };
+
     //
     // $scope.loadProducts = function () {
     //     $http.get('http://localhost:8189/market-core/api/v1/products')
