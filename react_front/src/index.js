b80699608e31e83097ac8d7ec51b6c477842cc9e
Torward <lomovfg@gmail.com>
2023-05-14 14:51:58 +1000

    Экстренный коммит II

--- react_front/src/index.js
+++
@@ -1,9 +0,0 @@
-
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import state from "./adapters/state";
-import {rerenderEntireTree} from "./adapters/render";
-
-
-rerenderEntireTree(state)
-reportWebVitals();
