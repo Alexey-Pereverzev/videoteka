b3c7630b1edf5875366221eb801ddb79cddddc54
serg <livegrafics@rambler.ru>
2023-05-25 09:09:12 +0300

    Fix AppError dtos

--- catalog-service/src/main/java/ru/gb/catalogservice/controllers/FilmController.java
+++ catalog-service/src/main/java/ru/gb/catalogservice/controllers/FilmController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.gb.api.dtos.dto.AppError;
 import ru.gb.api.dtos.dto.FilmDto;
 import ru.gb.api.dtos.dto.MinMaxYearDto;
 import ru.gb.api.dtos.dto.PageFilmDto;
