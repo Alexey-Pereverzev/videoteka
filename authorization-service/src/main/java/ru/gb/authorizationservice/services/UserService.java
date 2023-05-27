cc601b4eb41d652ab89999031dad3006778143a7
alexey_pereverzev <aper-hari@list.ru>
2023-05-27 17:26:31 +0600

    task18_2 - доработки валидации - проверки isBlank, isEmpty, null

--- authorization-service/src/main/java/ru/gb/authorizationservice/services/UserService.java
+++ authorization-service/src/main/java/ru/gb/authorizationservice/services/UserService.java
@@ -135,7 +135,7 @@ public class UserService implements UserDetailsService {
         }
 
 
-        if (phoneNumber==null || phoneNumber.isEmpty() || phoneNumber.isBlank()) {
+        if (phoneNumber==null || phoneNumber.isBlank()) {
             user.setPhoneNumber(null);
         } else {
             if (!validationService.acceptablePhoneNumber(phoneNumber)) {
@@ -144,7 +144,7 @@ public class UserService implements UserDetailsService {
             user.setPhoneNumber(phoneNumber);
         }
 
-        if (address==null || address.isEmpty() || address.isBlank()) {
+        if (address==null || address.isBlank()) {
             user.setAddress(null);
         } else {
             user.setAddress(address);
