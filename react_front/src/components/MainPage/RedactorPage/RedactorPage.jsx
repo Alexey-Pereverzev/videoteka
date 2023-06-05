b75f6cf4fd5a87e1d15bd2fcd34c1607d86ce669
Torward <lomovfg@gmail.com>
2023-06-05 18:54:27 +1000

    Работа с редакторской, промежуточный коммит.

--- react_front/src/components/MainPage/RedactorPage/RedactorPage.jsx
+++ react_front/src/components/MainPage/RedactorPage/RedactorPage.jsx
@@ -1,9 +1,11 @@
 import style from "./RedactorPage.module.css"
 import React, {createRef, useEffect, useRef, useState} from "react";
-import {Button, ButtonGroup} from "@mui/material";
+import {Button, ButtonGroup, FormControl, InputLabel, MenuItem, Select} from "@mui/material";
 import {SelectButton} from "primereact/selectbutton";
 import {SelectButtonChangeEvent} from "primereact/selectbutton";
 import axios from "axios";
+import {PlusIcon} from "primereact/icons/plus";
+import {toast} from "react-toastify";
 
 // Long id;
 // String title;
@@ -34,6 +36,7 @@ const RedactorPage = () => {
     }
     useEffect(() => {
         getGenres()
+        getAllFilms()
     },[])
 
 
@@ -41,19 +44,85 @@ const RedactorPage = () => {
     const [value, setValue] = useState([])
     const [directors, setDirectors] = useState([])
     const [countries, setCountries] = useState([])
+    const [open, setOpen] = useState(false)
+    const [films, setFilms] = useState([])
+
+
+    async function getAllFilms() {
+        try {
+            return await axios.get('http://localhost:5555/catalog/api/v1/film/list_all',{
+                params:{
+                    currentPage: 1,
+                    filterCountryList: '',
+                    filterDirectorList: '',
+                    filterGenreList: '',
+                    startPremierYear: '',
+                    endPremierYear: '',
+                    isSale: '',
+                    minPrice: '',
+                    maxPrice: '',
+                }
+            }).then((response) => {
+                    setFilms(response.data)
+                },
+                function errorCallback(response) {
+                    console.log(response)
+                    let displayCartNotification = (message) => {
+                        toast.error(message);
+                    }
+                    displayCartNotification(response.response.data.value)
+                })
+
+        } catch (e) {
+
+        }
+    }
+
+    function handleFilmsChange(event) {
+        console.log(event.target.value)
+        if (event.target.value === "Добавить фильм") {
+            setOpen(true)
+        } else {
+            if (event.target.value !== "Добавить фильм") {
+               setOpen(false)
+            }
+        }
+    }
 
     return (
         <div className={style.redactor_container}>
             Редактирование карточки фильма
             <div className={style.redactor__box}>
                 <div className={style.redactor__img_container}>
-                    <div className={style.input_container}>
-                        <input type="text" required=""/>
-                        <label>Ссылка для обложки</label>
-                    </div>
+                    <FormControl sx={{m: 1, minWidth: 120}} size="small" focused={false}>
+                        <InputLabel id="demo-select-small-label">Фильмы</InputLabel>
+                        <Select
+                            labelId="demo-select-small-label"
+                            id="demo-select-small"
+                            name={'director'}
+                            value={films}
+                            color={'success'}
+                            defaultValue={''}
+                            sx={{backgroundColor: 'darkgrey', borderColor: "success"}}
+                            label="Age"
+                            onChange={(event) => handleFilmsChange(event)}
+                        >
+                            <MenuItem value="Добавить фильм">
+                                <em><PlusIcon/>Добавить фильм</em>
+                            </MenuItem>
+                            {films.map((film) =>
+                                <MenuItem
+                                    value={film.title}>{film.title}</MenuItem>
+                            )}
+                        </Select>
+                    </FormControl>
                 </div>
                 <div className={style.details}>
                     <div className={style.title_input}>
+                        <div className={style.input_container}>
+                            <input type="text" required=""/>
+                            <label>Ссылка для обложки</label>
+                        </div>
                         <div className={style.input_container}>
                             <input type="text" required=""/>
                             <label>Название фильма</label>
