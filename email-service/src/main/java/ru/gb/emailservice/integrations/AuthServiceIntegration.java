e475308a53549476ff67a5e0988d6bb2aef3dfa4
alexey_pereverzev <aper-hari@list.ru>
2023-06-08 19:47:32 +0600

    task24_2 - безопасность на сервисе авторизации

--- email-service/src/main/java/ru/gb/emailservice/integrations/AuthServiceIntegration.java
+++ email-service/src/main/java/ru/gb/emailservice/integrations/AuthServiceIntegration.java
@@ -15,7 +15,7 @@ public class AuthServiceIntegration {
 
     public UserNameMailDto findById(Long id) {
         UserNameMailDto userNameMailDto = authServiceWebClient.get()
-                .uri("/api/v1/users/get_name_and_email_by_id/?id="+id)
+                .uri("/api/v1/users/name_and_email_by_id/?id="+id)
                 .retrieve()
                 .onStatus(HttpStatus::is5xxServerError,
                         error -> Mono.error(new RuntimeException("Server is not responding")))
