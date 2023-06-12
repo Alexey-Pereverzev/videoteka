0389fdcc48b96b6acf652723e3dbac7ed1ae69fc
alexey_pereverzev <aper-hari@list.ru>
2023-06-12 19:35:14 +0600

    task23_2_2 - смена пароля - в процессе тестирования

--- authorization-service/src/main/java/ru/gb/authorizationservice/repositories/PasswordChangeAttemptRepository.java
+++ authorization-service/src/main/java/ru/gb/authorizationservice/repositories/PasswordChangeAttemptRepository.java
@@ -1,13 +1,8 @@
 package ru.gb.authorizationservice.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.gb.authorizationservice.entities.PasswordChangeAttempt;
-
-import java.util.Optional;
 
 @Repository
 public interface PasswordChangeAttemptRepository extends JpaRepository<PasswordChangeAttempt, Long> {
